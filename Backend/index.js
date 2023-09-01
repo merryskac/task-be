c582350ae5edcbf9965e825bfe07189a57e6ed8e
unknown <merryskachr@gmail.com>
2023-09-01 14:50:32 +0800

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(
     saveUnitialized: false,
     secret: "session",
     cookie: {
-      maxAge: 1000 * 60 * 60,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
       sameSite: "lax",
       secure: false
     }
